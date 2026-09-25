0a87e6d3af50f03934e27346bdb150e463c10eef
Charayaphan Nakorn Boon Han <charayaphan.nakorn.boon.han@gmail.com>
2026-09-25 17:08:33 +0800

    Update setup.bash to include new environment variables
    Added additional cache directories for XDG and MuJoCo.

--- models/setup.bash
+++ models/setup.bash
@@ -1,4 +1,4 @@
-set -x 
+set -x
 
 export CACHE_PATH="/tier2/persistent_data/boonhan"
 export UV_CACHE_DIR="$CACHE_PATH/uv_cache"
@@ -10,5 +10,9 @@ export TRITON_CACHE_DIR="$CACHE_PATH/triton_cache"
 export TORCHINDUCTOR_CACHE_DIR="$CACHE_PATH/triton_cache/inductor"
 export VLLM_CACHE_ROOT="$CACHE_PATH/triton_cache/vllm"
 export CUDA_CACHE_PATH="$CACHE_PATH/triton_cache/nv"
+export XDG_CACHE_HOME="$CACHE_PATH/xdg_cache"
+export XDG_DATA_HOME="$CACHE_PATH/xdg_data"
+export UV_PYTHON_INSTALL_DIR="$CACHE_PATH/uv_python"
+export MUJOCO_PATH="$CACHE_PATH/mujoco"
 
 set +x
