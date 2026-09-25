0d6d92a8ef0662bb3df0d4bfedf3a371d701a9ea
Charayaphan Nakorn Boon Han <charayaphan.nakorn.boon.han@gmail.com>
2026-09-25 17:15:33 +0800

    Update setup.bash

--- models/setup.bash
+++ models/setup.bash
@@ -13,6 +13,8 @@ export XDG_CACHE_HOME="$CACHE_PATH/xdg_cache"
 export XDG_DATA_HOME="$CACHE_PATH/xdg_data"
 export UV_PYTHON_INSTALL_DIR="$CACHE_PATH/uv_python"
 export MUJOCO_PATH="$CACHE_PATH/mujoco"
+export MS_ASSET_DIR="$CACHE_PATH/maniskill"
+export LIBERO_CONFIG_PATH="$CACHE_PATH/libero"
 
 set +x
 export HF_TOKEN=""
